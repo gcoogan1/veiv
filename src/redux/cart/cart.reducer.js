e6dd82c23e0e8a064f3409810b88a5115b7041ad
Genevieve Coogan <gcoogan1@gmail.com>
2021-02-01 12:33:06 -0500

    checkout functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,29 +1,44 @@
-import {CartActionTypes} from './cart.types';
-import {addItemToCart} from './cart.utils';
+import { CartActionTypes } from "./cart.types";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const intialState = {
-    hidden: true,
-    cartItems: []
-  };
-  
-  const cartReducer = (state = intialState, action) => {
-    switch (action.type) {
-      case CartActionTypes.TOGGLE_CART_HIDDEN:
-        return{
-              ...state,
-              hidden: !state.hidden
-        };
-      case CartActionTypes.ADD_ITEM:
-        return{
-              ...state,
-             // cartItems: [...state.cartItems, action.payload]
-             cartItems: addItemToCart(state.cartItems, action.payload)
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
-  
-  export default cartReducer;
+  hidden: true,
+  cartItems: []
+};
+
+const cartReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+      
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        // cartItems: [...state.cartItems, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          //compares the cartItem id to the id we want to remove => if the ids do not match then keep the cartItem; if they ids match then remove the cartItem
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,18 +1,36 @@
-
 //addItemToCart takes in existing state and the new action payload(new item)
 //it checks if the cart item id matches any item that may be in the cart already
 //if there is a match the quantity is increased by one
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existsingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToAdd.id
-        )
-    if(existsingCartItem) {
-        return cartItems.map(cartItem => 
-                cartItem.id === cartItemToAdd.id 
-                ? {...cartItem, quantity: cartItem.quantity + 1} 
-                : {cartItem}
-            )
-    }
+  const existsingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+  if (existsingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : { cartItem }
+    );
+  }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1 }]
-}
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  //If the item quantity is 1, then remove just remove it
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id); //only keeps the cartItems that do not match the item that needs to be removed
+  }
+
+  //Otherwise map through the quantity and decrease by one
+  return cartItems.map(
+    cartItem =>
+      cartItem.id === cartItemToRemove.id //IF the ids match, then decrease by one
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem //Otherwise just return the cartItem bc it does not need to be removed
+  );
+};
